e5d65eabaa5d5aeba3f104afcff902b73e39da0f
Sumit Pramanik <supra.gr700@gmail.com>
2026-05-26 13:02:36 +0530

    Added audio extraction feature

--- process_video.py
+++ process_video.py
@@ -1,2 +1,16 @@
 #converting the videos to mp3 files
-import whisper
+import os
+import subprocess
+
+files = os.listdir("videos")
+
+for file in files:
+    print("Processing:", file)
+    video_path = os.path.join("videos", file)
+    audio_name = os.path.splitext(file)[0] + ".mp3"
+    audio_path = os.path.join("audios", audio_name)
+
+    command = ["ffmpeg", "-i", video_path, audio_path]
+    subprocess.run(command)
+    print("All videos converted!")
+   
